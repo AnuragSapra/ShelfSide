385633df7daec8099ceb9e6212dd1aa80d7a8f62
Anurag Sapra <anuragsapra951@gmail.com>
2026-07-28 02:41:24 +0530

    Add CLIENT_URL env var

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ mongoose
 //Middlewares
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
     credentials: true,
   }),
 );
@@ -38,7 +38,6 @@ app.use("/api/auth", authRouter);
 app.use("/api/books", bookRouter);
 app.use("/api/members", memberRouter);
 app.use("/api/borrows", borrowRouter);
-//Error Handler
 
 //Listener
 const PORT = process.env.PORT || 5000;
